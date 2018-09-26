bdbbe352dc02ab1d7e5f24df1c83a3790ececad5
NathanielHoeferEdu <nathaniel.d.hoefer@gmail.com>
2018-09-25 19:31:57 -0500

    Mod: Diff successful test run

--- developers.py
+++ developers.py
@@ -1,18 +1,45 @@
 """Module containing the classes for evaluating developer comments."""
 
+import re
+
+
 class DevProcessing:
     pass
+
 
 class Developer:
     pass
 
+
 class Diff:
+    """Given a standard diff, extracts modified lines and C++ comments."""
+
+    DIFF_PATTERN = "(?:^\+|^\-)(.*$)"
+    CPP_COMMENT_PATTERN = """(//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'""" \
+                          """|"(?:\\.|[^\\"])*")"""
+
     def __init__(self, diff):
         self._diff = diff
+        self._modified_lines = self._extract_mod_lines()
+        self._comments = self._extract_cpp_comments()
 
     @property
     def diff(self):
         return self._diff
 
+    @property
     def comments(self):
-        return [""]
+        return self._comments
+
+    @property
+    def modified_lines(self):
+        return self._modified_lines
+
+    def _extract_mod_lines(self):
+        mod_lines = re.findall(self.DIFF_PATTERN, self.diff, re.MULTILINE)
+        return "\n".join(mod_lines)
+
+    def _extract_cpp_comments(self):
+        comments = re.findall(self.CPP_COMMENT_PATTERN, self._modified_lines,
+                              re.DOTALL | re.MULTILINE)
+        return comments
--- tests/test_developers.py
+++ tests/test_developers.py
@@ -4,14 +4,15 @@ import unittest
 
 from developers import Diff
 
+
 class TestDiff(unittest.TestCase):
     DIFF_STD = '''diff --git a/README.md b/README.md\n''' \
                '''index 1a674ef..88ec755 100644\n''' \
                '''--- a/README.md\n''' \
                '''+++ b/README.md\n''' \
                '''@@ -15,7 +15,7 @@ pip install pypeln\n''' \
-               '''-With Pypeline you can easily create multi-stage data pipelines using 3 type of workers:\n''' \
-               '''+With Pypeline you can create multi-stage data pipelines using with 3 type of workers:'''
+               '''-With Pypeline you can easily create multi-stage\n''' \
+               '''+With Pypeline you can create multi-stage data:'''
 
     DIFF_SINGLE = "+// Updated single Line Comment in C++\n" \
                   "+price = price * 1.05\n" \
@@ -21,25 +22,26 @@ class TestDiff(unittest.TestCase):
                   "-price = price * 1.05 // updated inline comment"
 
     DIFF_INLINE_BLOCK = "Inline block comment /* inline block comment */\n" \
-                  "-Inline block comment /* updated inline block comment */"
+                        "-Inline block comment /* updated inline block comment */"  # nopep8
 
     DIFF_ONE_LINE_BLOCK = '''def quicksort():\n''' \
                           '''+/* Updated Single line block comment */\n''' \
                           '''/* Single line block comment */\n'''
 
     DIFF_MULTI_LINE_BLOCK = '''def quicksort():\n''' \
-                        '''/* This is multi line block comment. \n''' \
-                        '''in C++\n''' \
-                        '''*/\n''' \
-                        '''-/* Updated multi line block comment. \n''' \
-                        '''-in C++\n''' \
-                        '''-*/\n'''
+                            '''/* This is multi line block comment. \n''' \
+                            '''in C++\n''' \
+                            '''*/\n''' \
+                            '''-/* Updated multi line block comment. \n''' \
+                            '''-in C++\n''' \
+                            '''-*/\n'''
 
     DIFF_BAD = '''def quicksort():\n''' \
-                '''-/* Updated multi line block comment. \n''' \
-                '''-in C++\n'''
+               '''-/* Updated multi line block comment. \n''' \
+               '''-in C++\n'''
 
-    DIFF_MULTIPLE = "\n".join([DIFF_ONE_LINE_BLOCK, "", DIFF_SINGLE, DIFF_INLINE])
+    DIFF_MULTIPLE = "\n".join([DIFF_ONE_LINE_BLOCK, "",
+                               DIFF_SINGLE, DIFF_INLINE])
 
     def test_diff_inst(self):
         diff = Diff(self.DIFF_STD)
@@ -47,30 +49,32 @@ class TestDiff(unittest.TestCase):
 
     def test_diff_block(self):
         diff = Diff(self.DIFF_SINGLE)
-        self.assertEqual(diff.comments(), ["// Updated single Line Comment in C++"])
+        self.assertEqual(diff.comments,
+                         ["// Updated single Line Comment in C++"])
 
     def test_diff_inline(self):
         diff = Diff(self.DIFF_INLINE)
-        self.assertEqual(diff.comments(), ["// updated inline comment"])
+        self.assertEqual(diff.comments, ["// updated inline comment"])
 
     def test_diff_inline_block(self):
         diff = Diff(self.DIFF_INLINE_BLOCK)
-        self.assertEqual(diff.comments(), ["/* updated inline block comment */"])
+        self.assertEqual(diff.comments, ["/* updated inline block comment */"])
 
     def test_diff_one_line_doc(self):
         diff = Diff(self.DIFF_ONE_LINE_BLOCK)
-        self.assertEqual(diff.comments(), ["/* Updated Single line block comment */"])
+        self.assertEqual(diff.comments,
+                         ["/* Updated Single line block comment */"])
 
     def test_diff_multi_line_doc(self):
         diff = Diff(self.DIFF_MULTI_LINE_BLOCK)
-        expected_comment = '''-/* Updated multi line block comment. \n''' \
-                           '''-in C++\n''' \
-                           '''-*/\n'''
-        self.assertEqual(diff.comments(), [expected_comment])
+        expected_comment = '''/* Updated multi line block comment. \n''' \
+                           '''in C++\n''' \
+                           '''*/'''
+        self.assertEqual(diff.comments, [expected_comment])
 
     def test_diff_bad(self):
         diff = Diff(self.DIFF_BAD)
-        self.assertEqual(diff.comments(), [])
+        self.assertEqual(diff.comments, [])
 
     def test_diff_multiple(self):
         diff = Diff(self.DIFF_MULTIPLE)
@@ -78,7 +82,7 @@ class TestDiff(unittest.TestCase):
         expected_comment.append('/* Updated Single line block comment */')
         expected_comment.append('// Updated single Line Comment in C++')
         expected_comment.append('// updated inline comment')
-        self.assertEqual(diff.comments(), expected_comment)
+        self.assertEqual(diff.comments, expected_comment)
 
 
 if __name__ == '__main__':
