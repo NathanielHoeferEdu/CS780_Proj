8c02f3bc4d25d13012b6e97f80475f0fd24ce18c
NathanielHoeferEdu <nathaniel.d.hoefer@gmail.com>
2018-09-27 09:45:52 -0500

    Add: Repo for unittesting

---
+++ tests/test_repo/test_file.cpp
@@ -0,0 +1,15 @@
+/**
+This is a test file
+/*
+
+// This is a class specifically for comments
+class Comments {
+
+public:
+
+    // This is a double!
+    double number;
+
+    /* Another comment by Billy */
+
+};
